29914f5ba1a87d25e50c25c4c7e2f7427e016300
mwuyan <mwuyan@yahoo.com>
2022-01-29 16:26:59 -0600

    commit 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/flask/hello').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +30,11 @@ function App() {
         >
           Learn React
         </a>
+
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
       </header>
     </div>
   );
