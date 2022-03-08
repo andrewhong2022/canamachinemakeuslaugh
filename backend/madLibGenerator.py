3779f9617cea14b834008a82dbe0c80c4f2a1fd1
andrewhong2022 <andrewhong2022@u.northwestern.edu>
2022-03-07 19:53:34 -0600

    use madLibGenerator

--- backend/madLibGenerator.py
+++ backend/madLibGenerator.py
@@ -1,6 +1,7 @@
 from flask_restful import Api, Resource, reqparse
 from backend.textGenerator import TextGenerator
 from backend.templateCreator import TemplateCreator
+import json
 
 class MadLibGenerator(Resource):
     def __init__(self, prompt = "Once upon a time"):
@@ -8,8 +9,12 @@ class MadLibGenerator(Resource):
         self.gen = TextGenerator(prompt)
 
     def get(self):
-        sample = self.gen.get()
+        sample = self.gen.generate()
         tempCreator = TemplateCreator(sample, removalFrequency=6)
         tempCreator.parse()
         tempCreator.createTemplate()
-        return tempCreator.returnPrompt() + tempCreator.returnTemplate()
+        prompt = " ".join(tempCreator.returnPrompt()) # TODO: FIX THIS SHIT so it's a clean sentence.
+        return json.dumps({
+            "prompt" : prompt,
+            "body" : tempCreator.returnTemplate()
+        })
