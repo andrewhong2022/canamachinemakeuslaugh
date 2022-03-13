be538d3a87328d6d721162d494ea7b224c32364f
andrewhong2022 <andrewhong2022@u.northwestern.edu>
2022-03-13 17:52:32 -0500

    plsplspls

--- frontend/src/MadLib.js
+++ frontend/src/MadLib.js
@@ -25,8 +25,23 @@ const MadLib = ({didStartState, newTextState, oldTextState, isDoneState}) => {
               newTextState.setNewTextWithBlanks(dataJSON.newSentence.slice(0,-1).join(" ").replaceAll(/ ([^"$A-Za-z0-9])/ig, '$1'));
             }
         }).catch(error => {
-            console.log(error)
-            isDoneState.setIsDone(true);
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
+          isDoneState.setIsDone(true);
         })
 
     // TODO: if get error message, finish
